f45167e6d57ea51c38c001648ebee93103eb1f83
DanielaSilvana <40808765+DanielaSilvana@users.noreply.github.com>
2018-07-10 17:35:43 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{ 	
+//Para el ejercicio es lo mismo nombre/localidad, compila bien//
+	var localidad;
+	localidad=prompt("Por favor, ingrese su localidad debajo:");
+	alert("Usted vive en la localidad:" + localidad );
+
+}
+
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,17 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{	//Dentro del prompt el ingreso y la respuesta//
+	var nombre;
+	nombre=prompt("Ingrese su nombre" , "Daniela");
+	nombre=document.getElementById("Ingrese su nombre").value;
+
+	}
+
+
+
+
+
+
