a5978cdcc96b23896c43ae4b10f4d882489df3df
DanielaSilvana <40808765+DanielaSilvana@users.noreply.github.com>
2018-07-10 22:02:13 -0300

    Add files via upload

---
+++ jsInstruccionIF (6).js
@@ -0,0 +1,31 @@
+function mostrar()
+{	//tomo la edad  
+	var edad
+	edad=document.getElementById('edad').value;
+	
+	/*Versión "Disney":
+
+	if(edad<12);
+		{	alert("Ud es un niño");
+		}
+
+			if(edad>12 && edad<18);
+			{	alert("Ud es un adolescente");
+			}
+
+				if(edad>18);
+					{alert("Ud es mayor");
+					}
+	
+	*/
+
+	/* Versión IO
+
+
+
+	
+	else()
+
+
+
+}	//FIN DE LA FUNCIÓN
