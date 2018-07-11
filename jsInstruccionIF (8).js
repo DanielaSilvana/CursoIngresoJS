75174fe77ecfce66bdbdb3dd87f6f849fc24c212
DanielaSilvana <40808765+DanielaSilvana@users.noreply.github.com>
2018-07-10 22:15:56 -0300

    Add files via upload

---
+++ jsInstruccionIF (8).js
@@ -0,0 +1,18 @@
+function mostrar()
+{//tomo la edad  
+
+/*Probar todas las posibilidades intercambiando variables y valores*/
+
+		
+	var edad;
+	var estadoCivil;
+
+	edad=document.getElementById('edad').value;
+	if(edad<18 && estadoCivil=Casado);
+		{
+			//NADA de nada
+		}
+/*Si NADA al ingresar edad<18 + estadoCivil distinto, implica...*/
+
+
+}	//FIN DE LA FUNCIÓN
